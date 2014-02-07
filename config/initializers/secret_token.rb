3e73f83e193b5061bf5f83298af27bf127852a47
Girish Bhagavath <girish.bhagavath@in.v2solutions.com.com>
2014-02-07 11:09:10 +0530

    All file

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Spreetest::Application.config.secret_token = 'f78ab6164f760873ad518c653f001f5534f4161309963bc507182b8e3ff328c93723f5cba171382f5ae5ac930bc82ae78d6af6929b669dc15ea2732535271704'
